d26dcfa41105ec0aed8a5b82fc62a32092d06d77
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2024-10-24 15:20:32 -0500

    init commit

---
+++ src/main/java/co/edu/unicauca/asae/tallerhexagonal/TallerhexagonalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.tallerhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerhexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerhexagonalApplication.class, args);
+	}
+
+}
